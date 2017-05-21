eb9e6cba3d108890b9d4d60a2345d6b9c1891b5e
martinabeleda <martin.abeleda@gmail.com>
2017-05-21 18:49:52 +1000

    Lane detection working

--- lane_follow/lanedetect.py
+++ lane_follow/lanedetect.py
@@ -30,6 +30,61 @@ def region_of_interest(img, vertices):
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
+"""
+This function draws `lines` with `color` and `thickness`.
+"""
+def draw_lines(img, lines, color=[255, 255, 0], thickness=4, thresh=0.15):
+
+    imshape = img.shape
+
+    # these variables represent the y-axis coordinates to which the line will be extrapolated to
+    ymin_global = img.shape[0]
+    ymax_global = img.shape[0]
+
+    # left lane line variables
+    all_left_grad = []
+    all_left_y = []
+    all_left_x = []
+
+    # right lane line variables
+    all_right_grad = []
+    all_right_y = []
+    all_right_x = []
+
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            gradient, intercept = np.polyfit((x1,x2), (y1,y2), 1)
+            ymin_global = min(min(y1, y2), ymin_global)
+
+            if (gradient > 0 + thresh):
+                all_left_grad += [gradient]
+                all_left_y += [y1, y2]
+                all_left_x += [x1, x2]
+            elif (gradient < 0 - thresh):
+                all_right_grad += [gradient]
+                all_right_y += [y1, y2]
+                all_right_x += [x1, x2]
+
+    left_mean_grad = np.mean(all_left_grad)
+    left_y_mean = np.mean(all_left_y)
+    left_x_mean = np.mean(all_left_x)
+    left_intercept = left_y_mean - (left_mean_grad * left_x_mean)
+
+    right_mean_grad = np.mean(all_right_grad)
+    right_y_mean = np.mean(all_right_y)
+    right_x_mean = np.mean(all_right_x)
+    right_intercept = right_y_mean - (right_mean_grad * right_x_mean)
+
+    # Make sure we have some points in each lane line category
+    if ((len(all_left_grad) > 0) and (len(all_right_grad) > 0)):
+        upper_left_x = int((ymin_global - left_intercept) / left_mean_grad)
+        lower_left_x = int((ymax_global - left_intercept) / left_mean_grad)
+        upper_right_x = int((ymin_global - right_intercept) / right_mean_grad)
+        lower_right_x = int((ymax_global - right_intercept) / right_mean_grad)
+
+        cv2.line(img, (upper_left_x, ymin_global), (lower_left_x, ymax_global), color, thickness)
+        cv2.line(img, (upper_right_x, ymin_global), (lower_right_x, ymax_global), color, thickness)
+
 # Read image in color
 #IMREAD_GRAYSCALE
 #IMREAD_COLOR
@@ -50,15 +105,22 @@ lowThreshold = 150
 highThreshold = 200
 edges = cv2.Canny(blur, lowThreshold, highThreshold)
 
-# Extract Region of Interest
+# Mask out areas outside region of interest
+# Region of interest is defined as a trapezoid to account for perspective
 lowerLeftPoint = [0, 600]
 middleLeftPoint = [0, 450]
 upperLeftPoint = [250, 300]
 upperRightPoint = [550, 300]
 middleRightPoint = [800, 450]
 lowerRightPoint = [800, 600]
-pts = np.array([[lowerLeftPoint, middleLeftPoint, upperLeftPoint, upperRightPoint, middleRightPoint, lowerRightPoint]], dtype=np.int32)
+pts = np.array([[lowerLeftPoint, middleLeftPoint, upperLeftPoint,
+                 upperRightPoint, middleRightPoint, lowerRightPoint]], dtype=np.int32)
 roi = region_of_interest(edges, pts)
+
+# Remove false negatives
+kernel = np.ones((5, 5), np.uint8)
+#dilation = cv2.dilate(roi,kernel,iterations = 1)
+closing = cv2.morphologyEx(roi, cv2.MORPH_CLOSE, kernel)
 
 # Hough Lines
 rho = 1
@@ -66,19 +128,19 @@ theta = np.pi/180
 threshold = 30
 minLineLength = 20
 maxLineGap = 20
-lines = cv2.HoughLinesP(roi, rho, theta, threshold, minLineLength, maxLineGap)
+lines = cv2.HoughLinesP(closing, rho, theta, threshold,
+                        minLineLength, maxLineGap)
+
+print(lines)
 
 # Display hough lines
-for x in range(0, len(lines)):
-    for x1,y1,x2,y2 in lines[x]:
-        cv2.line(img,(x1,y1+300),(x2,y2+300),(0,255,0), thickness=2)
+draw_lines(img, lines)
 
 # Display center line
-#cv2.line(img, (1296, 0), (1296, 1944), (255, 0, 0), thickness=2)
 cv2.line(img, (400, 0), (400, 600), (255, 0, 0), thickness=2)
 
 # Display
-cv2.imshow('image', roi)
+cv2.imshow('image', img)
 cv2.imshow('canny', edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
