e8b41d10a78c011ad3361ddeb0464e777afb6a83
martinabeleda <martin.abeleda@gmail.com>
2017-05-21 13:08:33 +1000

    hough tuning

--- lane_follow/README.md
+++ lane_follow/README.md
@@ -1,1 +1,2 @@
 # Lane Following Module
+The lane following module implements lane detection using the Python OpenCV API. This module also sends motor control commands based on the position of the robot within the lanes. 
--- lane_follow/lanedetect.py
+++ lane_follow/lanedetect.py
@@ -5,6 +5,31 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+Applies an image mask.
+
+Only keeps the region of the image defined by the polygon
+formed from `vertices`. The rest of the image is set to black.
+"""
+def region_of_interest(img, vertices):
+
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)
+
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+
+    #filling pixels inside the polygon defined by "vertices" with the fill color
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
 # Read image in color
 #IMREAD_GRAYSCALE
 #IMREAD_COLOR
@@ -12,19 +37,36 @@ import matplotlib.pyplot as plt
 img = cv2.imread('images/2017-05-19_120235.jpg', cv2.IMREAD_COLOR)
 #img = cv2.imread('images/2017-05-19_120425.jpg', cv2.IMREAD_COLOR)
 
-# Extract ROI and resize
-roi = img[972:1944, 0:2592]
-roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+# Resize and convert to grayscale
 img = cv2.resize(img, (800, 600))
-roi = cv2.resize(roi, (800, 300))
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Gaussian Blur
+kernelSize = 5
+blur = cv2.GaussianBlur(gray, (kernelSize,kernelSize), 0)
 
-# Line Detection
-blur = cv2.GaussianBlur(roi,(5,5),0)
-edges = cv2.Canny(blur, threshold1=100, threshold2=200)
-minLineLength = 30
-maxLineGap = 10
-lines = cv2.HoughLinesP(edges, 1, np.pi/180, 40, minLineLength, maxLineGap)
+# Canny Edge Detection
+lowThreshold = 150
+highThreshold = 200
+edges = cv2.Canny(blur, lowThreshold, highThreshold)
+
+# Extract Region of Interest
+lowerLeftPoint = [0, 600]
+middleLeftPoint = [0, 450]
+upperLeftPoint = [250, 300]
+upperRightPoint = [550, 300]
+middleRightPoint = [800, 450]
+lowerRightPoint = [800, 600]
+pts = np.array([[lowerLeftPoint, middleLeftPoint, upperLeftPoint, upperRightPoint, middleRightPoint, lowerRightPoint]], dtype=np.int32)
+roi = region_of_interest(edges, pts)
+
+# Hough Lines
+rho = 1
+theta = np.pi/180
+threshold = 30
+minLineLength = 20
+maxLineGap = 20
+lines = cv2.HoughLinesP(roi, rho, theta, threshold, minLineLength, maxLineGap)
 
 # Display hough lines
 for x in range(0, len(lines)):
@@ -36,7 +78,7 @@ for x in range(0, len(lines)):
 cv2.line(img, (400, 0), (400, 600), (255, 0, 0), thickness=2)
 
 # Display
-cv2.imshow('image', img)
+cv2.imshow('image', roi)
 cv2.imshow('canny', edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
