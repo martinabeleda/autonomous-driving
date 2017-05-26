1f3ade1be3774155de201d9a38958fac01aa2d30
martinabeleda <martin.abeleda@gmail.com>
2017-05-26 14:45:39 +1000

    added mask display window

--- lane_follow/lane_detect.py
+++ lane_follow/lane_detect.py
@@ -53,8 +53,7 @@ def lane_detect(img):
     lines = cv2.HoughLinesP(closing, rho, theta, threshold,
                           minLineLength, maxLineGap)
 
-    #masked = cv2.cvtColor(closing, cv2.COLOR_CV_GRAY2RGB)
-    masked = closing
+    masked = region_of_interest(img, pts)
 
     for line in lines:
         for x1,y1,x2,y2 in line:
--- lane_follow/pi_lane_follow.py
+++ lane_follow/pi_lane_follow.py
@@ -27,19 +27,19 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	# and occupied/unoccupied text
 	image = frame.array
 
-        (detected, lines, angle, displacement) = lane_detect(image)
-        
+        (detected, masked, angle, displacement) = lane_detect(image)
+
 	# show the frame
 	cv2.imshow("Frame", detected)
+    cv2.imshow("Masked", masked)
 	key = cv2.waitKey(1) & 0xFF
 
         print angle
         print displacement
-        
+
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
 
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
-    
