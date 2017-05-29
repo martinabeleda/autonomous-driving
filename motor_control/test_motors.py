8faf9d97e27abee2919335c2ba114043a24a16b6
martinabeleda <martin.abeleda@gmail.com>
2017-05-29 21:33:18 +1000

    test

--- motor_control/test_motors.py
+++ motor_control/test_motors.py
@@ -1,6 +1,16 @@
 from motors import *
 from time import sleep
 
+GPIO.setmode(GPIO.BOARD)
+
+Motor1A = 16
+Motor2A = 18
+Motor12EN = 22
+
+Motor3A = 11
+Motor4A = 13
+Motor34EN = 15
+
 motor_setup()
 
 sleep(1)
