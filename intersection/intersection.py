057dc1c5b9d3c1d63735a4b72b54ec98c6f1b63b
Kristina-M <echidnalover96@gmail.com>
2017-05-30 12:23:54 +1000

    added interesection test and intersect stub functions

--- intersection/intersection.py
+++ intersection/intersection.py
@@ -1,8 +1,23 @@
-#imports
+#import
+import cv2
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+from datetime import datetime 
+
+from region_of_interest import region_of_interest
+
 
 
 def is_red_line(image)
+	
+	
+	return maskedImage, red_flag
+
 
 def read_barcode(maskedImage)
+
+
+	return codeNumber
 
 def check_light()
--- intersection/test_intersection.py
+++ intersection/test_intersection.py
@@ -8,3 +8,70 @@ import numpy as np
 from intersection import is_red_line, read_barcode, check_light
 
 
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (800, 600)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(800, 600))
+camera.vflip = True
+camera.hflip = True
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+#defines
+RED = 1
+
+# Main loop
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    	# grab the raw NumPy array representing the image, then initialize the timestamp
+		# and occupied/unoccupied text
+		image = frame.array
+
+		# gaussian blur
+	    kernelSize = 5
+	    blur = cv2.GaussianBlur(gray, (kernelSize,kernelSize), 0)
+
+		#Check to see  if red line is present - enter intersection module 
+		maskedImage, line = is_red_line(image)
+
+		if line is RED:
+
+			#if at intersection 
+			turnCode = read_barcode(maskedImage)
+			print("Turn Code", turnCode)
+
+			#Move forwards
+			print("Move Forwards")
+
+			#wait and exit on green light
+			check_light()
+			print("Light is green")
+
+			turn_decide(turnCode)
+
+		else
+			print("Not an intersection")
+
+
+		# show the frame
+		cv2.imshow("Frame", img)
+    	key = cv2.waitKey(1) & 0xFF
+
+		# clear the stream in preparation for the next frame
+		rawCapture.truncate(0)
+
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
+
+
+
+
+def turn_decide(turnCode)
+	
+
+	print("Turn Left")
+
+	
