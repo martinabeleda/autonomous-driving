9f3572762b12e23befe567dbdff536ec8b037087
Riley Green <rgre2543@uni.sydney.edu.au>
2017-06-09 02:16:19 +1000

    Create path_planning.py

--- path_planning.py
+++ path_planning.py
@@ -1,12 +1,13 @@
 import urllib2
 
-print "Would you like to recieve the driving commands from base station? (Y/N)"
-while 1:
-    	inkey = raw_input()
-    	if inkey is "Y": 
-		target_url = "http://54.153.238.139/exp/commands.txt"
-		for line in urllib2.urlopen(target_url):
-    			print line
-		break
-	if inkey is "N": break
+def baseStation_Commands():
+	print "Would you like to recieve the driving commands from base station? (Y/N)"
+	while 1:
+		inkey = raw_input()
+		if inkey is "Y": 
+			target_url = "http://54.153.238.139/exp/commands.txt"
+			for line in urllib2.urlopen(target_url):
+				print line
+			break
+		if inkey is "N": break
 
