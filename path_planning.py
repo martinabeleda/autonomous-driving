38d368d3b7d36a4c1048ad5948e1dcb696ba12c4
Riley Green <rgre2543@uni.sydney.edu.au>
2017-06-09 03:23:18 +1000

    Add files via upload

--- path_planning.py
+++ path_planning.py
@@ -1,13 +1,12 @@
 import urllib2
 
-def baseStation_Commands():
-	print "Would you like to recieve the driving commands from base station? (Y/N)"
-	while 1:
-		inkey = raw_input()
-		if inkey is "Y": 
-			target_url = "http://54.153.238.139/exp/commands.txt"
-			for line in urllib2.urlopen(target_url):
-				print line
-			break
-		if inkey is "N": break
+print "Would you like to recieve the driving commands from base station? (Y/N)"
+while 1:
+    	inkey = raw_input()
+    	if inkey is "Y": 
+		target_url = "http://54.153.238.139/exp/commands.txt"
+		for line in urllib2.urlopen(target_url):
+    			print line
+		break
+	if inkey is "N": break
 
